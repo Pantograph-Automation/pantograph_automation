3b78444cb6f8c41aaae0f969a0df75df2f7624f1
dmalexa5 <dmalexa5@ncsu.edu>
2025-11-11 13:33:36 -0500

    Add demo script

---
+++ examples/demo_11-14-2025.py
@@ -0,0 +1,23 @@
+from pantograph_control import drivers, kinematics
+
+
+# Set up the right stepper
+ENA1 = 17
+PUL1 = 27
+DIR1 = 22
+
+
+# Set up the left stepper
+ENA2 = 11
+PUL2 = 13
+PUL2 = 15
+
+# Set up the servo
+SERVO_PIN = 16
+
+# Full ROM
+
+
+# Callus transfer simulation
+
+# Dish transfer simulation
