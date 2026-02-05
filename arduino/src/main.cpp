a2362479333e0989a8f6ef4938f34cd8a00c8965
David Alexander <dmalexa5@ncsu.edu>
2026-02-05 14:22:04 -0500

    Unbuilt rt stepper control

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,48 +1,23 @@
-#include <AccelStepper.h>
-#include <Encoder.h>
 #include <AS5600.h>
+#include "control.hpp"
 #include "config.h"
 
 // usbipd list
 // usbipd attach --wsl --busid 2-1
 
 const float POS_CMD_MAX_ERROR = 5 * RAD_PER_STEP;
-float error = 0.0f;
-float prev_error = 0.0f;
 
 // Encoder E1(E1_A_PIN, E2_B_PIN);
 AS5600 as5600;
 
+// Stepper 1
+Stepper stepper(S1_PULSE_PIN, S1_ENABLE_PIN, S1_DIR_PIN);
+
+Controller controller(stepper);
+
 uint32_t clk = 0;
 
-uint32_t start, stop;
-float offset;
-bool pulse_active = false;
-unsigned long pulse_start = 0UL;
-unsigned long last_time = 0UL;
-// PID loop variables
-float pos_cmd = 0.0;
-float pos_est = 0.0f;
-float vel_prev = 0.0f;
-
 const float desired_position = 1.570796; // 90 degrees
-
-void enable_stepper() {
-  digitalWrite(S1_ENABLE_PIN, HIGH);
-}
-
-void disable_stepper() {
-  digitalWrite(S1_ENABLE_PIN, LOW);
-}
-void set_direction_forward()
-{
-  digitalWrite(S1_DIR_PIN, HIGH);
-}
-
-void set_direction_backward()
-{
-  digitalWrite(S1_DIR_PIN, LOW);
-}
 
 float calibrate(const int sample_size, const unsigned long ms_delay)
 {
@@ -76,10 +51,6 @@ void setup()
   Wire.begin();
   Wire.setClock(400000);
 
-  pinMode(S1_PULSE_PIN, OUTPUT);
-  pinMode(S1_ENABLE_PIN, OUTPUT);
-  pinMode(S1_DIR_PIN, OUTPUT);
-
   as5600.begin(4);  //  set direction pin.
 
   as5600.setDirection(AS5600_CLOCK_WISE);  //  default, just be explicit.
@@ -87,13 +58,13 @@ void setup()
   Serial.print("Connect: ");
   Serial.println(b);
 
-  disable_stepper();
+  stepper.disable();
   Serial.println("Calibrating...");
   delay(2000);
   offset = calibrate(100, 20);
   Serial.println("Calibrated!! Move the arm.");
   delay(2000);
-  enable_stepper();
+  stepper.enable();
   
 }
 
@@ -101,64 +72,33 @@ void setup()
 void loop()
 {
   
-  unsigned long now = micros();
-  unsigned long elapsed_us = now - last_time;
-  float dt = elapsed_us * 1e-6f; // seconds
-  last_time = now;
+  // Pulse the stepper motor if required
+  stepper.pulse_if_required(PULSE_WIDTH_US);
 
-  // ---- Pulse width housekeeping (non-blocking) ----
-  if (pulse_active && (now - pulse_start >= PULSE_WIDTH_US)) {
-    digitalWrite(S1_PULSE_PIN, LOW);
-    pulse_active = false;
-  }
-
-  // ---- read encoder for PID (measured position) ----
+  // Get the current measured position
   float current_angle = read_encoder(offset);
-  // ---- PID
-  error = desired_position - current_angle;
 
-  // deadband around target
-  if (fabs(error) < POS_DEADBAND) {
-    error = 0.0f;
-  }
+  stepper.update_state(current_angle, micros());
 
-  float vel_cmd = Kp * error;
 
-  if (vel_cmd - vel_prev > ACCEL_MAX) vel_cmd = vel_prev + ACCEL_MAX;
-  if (vel_cmd - vel_prev < -ACCEL_MAX) vel_cmd = vel_prev - ACCEL_MAX;
-  
-  vel_prev = vel_cmd;
+  /**
+   * Other code goes here
+   */
 
-  if (vel_cmd > V_MAX) vel_cmd = V_MAX;
-  if (vel_cmd < -V_MAX) vel_cmd = -V_MAX;
 
-  // ---- integrate velocity to get a commanded continuous position ----
-  pos_cmd += vel_cmd * dt;
+  // Run the PID law to determine whether a step is required
+  Controller::Status status = controller.pid(
+    Kp, Ki, Kd,
+    desired_position,
+    V_MAX,
+    ACCEL_MAX,
+    POS_CMD_MAX_ERROR,
+    POS_DEADBAND,
+    RAD_PER_STEP
+  );
 
-  float cmd_err = pos_cmd - pos_est;
-  if (cmd_err > POS_CMD_MAX_ERROR)
-      pos_cmd = pos_est + POS_CMD_MAX_ERROR;
-  else if (cmd_err < -POS_CMD_MAX_ERROR)
-      pos_cmd = pos_est - POS_CMD_MAX_ERROR;
-
-  // ---- decide whether to step, using internal pos_est (not raw encoder) ----
-  float delta = pos_cmd - pos_est;
-
-  if (!pulse_active) {
-    if (delta >= RAD_PER_STEP) {
-      // forward step
-      set_direction_forward();  // set correct direction polarity for your hardware
-      digitalWrite(S1_PULSE_PIN, HIGH);
-      pulse_start = now;
-      pulse_active = true;
-      pos_est += RAD_PER_STEP;         // update internal estimate immediately
-    } else if (delta <= -RAD_PER_STEP) {
-      // backward step
-      set_direction_backward();
-      digitalWrite(S1_PULSE_PIN, HIGH);
-      pulse_start = now;
-      pulse_active = true;
-      pos_est -= RAD_PER_STEP;
-    }
+  if (status == Controller::Status::FINISHED)
+  {
+    Serial.println("Finished moving to setpoint!");
   }
 }
